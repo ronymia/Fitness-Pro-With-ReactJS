2f15eb2078cc0db948e45f3ed064753795eecdd4
MD Rony Mia <official.mdronymia@gmail.com>
2022-04-19 15:58:17 +0600

    about done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Register from './Components/Register/Register';
 import RequireAuth from './Components/RequireAuth/RequireAuth';
 import CheckOutBook from './Components/CheckOutBook/CheckOutBook';
 import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         }
         ></Route>
         <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/about' element={<About />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/*' element={<NotFound />}></Route>
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,9 +3,13 @@ import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>it's me broooooooooo!</h1>
-        </div>
+        <section className='flex justify-center items-center text-center'>
+            <p className='w-2/4 p-8'>
+                আমি নাজমুল মাহমুদ রনি । আমার goal হলো আগামী ২/৩ মাসের মধ্যে একটা কোম্পানিতে চাকরি করার ability অর্জন করা । এজন্য আমি প্রতি দিন আমাএ সর্বোচ্চ দিয়ে  Practice করি ।
+                আমি একটা Online Food Delivery Company  তে Ride দেই।
+                আমার স্বপ্ন আমি একটা software Company তে job করবো।
+            </p>
+        </section>
     );
 };
 
