c369d2892b1197ec7f081b85325b542d64004b9c
MD Rony Mia <official.mdronymia@gmail.com>
2022-04-19 15:34:42 +0600

    social login design

--- src/Components/Shared/SocialLogin/SocialLogin.js
+++ src/Components/Shared/SocialLogin/SocialLogin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import googleLogo from '../../../images/googleLogo.png'
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
@@ -10,13 +11,6 @@ const SocialLogin = () => {
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
 
-    if (error) {
-        return (
-            <div>
-                <p>Error: {error.message}</p>
-            </div>
-        );
-    }
     if (loading) {
         return <p>Loading...</p>;
     }
@@ -25,10 +19,22 @@ const SocialLogin = () => {
     }
 
     return (
-        <div>
+        <div className=''>
+            <div className="flex justify-center items-center gap-2 my-4">
+                <div className="bg-gray-300 h-[1px] w-2/5"></div>
+                <p>or</p>
+                <div className="bg-gray-300 h-[1px] w-2/5"></div>
+            </div>
             <button
                 onClick={() => signInWithGoogle()}
-            >Continue With Google</button>
+                className="bg-yellow-600 w-full rounded-md h-[80px] flex justify-center items-center">
+                <div className="flex items-center">
+                    <img className='h-[100px]' src={googleLogo} alt="" />
+                    <div className="">
+                        <span className='text-2xl text-white'>Continue With Google</span>
+                    </div>
+                </div>
+            </button>
         </div>
     );
 };
